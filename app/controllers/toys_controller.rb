6479368b58df84834bf96bdc3dad427f3e6fa202
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-04-20 12:17:49 +1000

    added create functions

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -11,6 +11,9 @@ class ToysController < ApplicationController
         @toy = Toy.find(params[:id])
     end
     
+    def new
+        
+    end
     #Create a new celebrity
     def create
         name = params[:name]
@@ -25,20 +28,14 @@ class ToysController < ApplicationController
     
     #Update a celebrity
     def update
-        @toy = Toy.find(params[:id])
+        updated_toy = Toy.update(params["id"], name: params["name"], description: params["description"], date: params["date"], user: params["user"])
 
-        @toy.name = params[:name]
-        @toy.description = params[:description]
-        @toy.date = params[:date]
-        @toy.user = params[:user]
-
-        @toy = Toy.find(params[:id])
-        render json: @toy
+        render json: updated_toy
     end
     
     #Remove a celebrity
     def destroy
-        @toy = Toy.find(params[:id]).destroy
+        @toy = Toy.find(params["id"]).destroy
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/toys", to: "toys#index", as: :home
+  get "/toys/new", to: "toys#new", as: :new_entry
   get "/toys/:id", to: "toys#show", as: :show
   post "/toys", to: "toys#create"
   put "/toys/:id", to: "toys#update"
